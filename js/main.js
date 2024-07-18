34d1f9931846ac2c7e4d963edc0d647c7eb4e94d
Kamal Adebayo <kamaladebayo10@gmail.com>
2024-07-18 15:11:59 +0100

    2024 revamp

--- js/main.js
+++ js/main.js
@@ -1,16 +1,77 @@
-// Sponsors slider
-const slider = document.querySelector('.slider');
-const logos = document.querySelectorAll('.slider img');
+// Hero Images
+const images = [
+    // './images/hero/hero-sm.webp',
+    // './images/hero/hero-sm.webp',
+    './images/hero/1.webp',
+    './images/hero/3.webp',
+    './images/hero/7.webp',
+    './images/hero/4.webp',
+    './images/hero/2.webp',
+    './images/hero/5.webp',
+    './images/hero/6.webp',
+];
 
-let index = 0;
+const imageElement = document.getElementById('hero-image');
+let currentIndex = 0;
 
-function slide() {
-    index++;
-    if (index >= logos.length) {
-        index = 0;
-    }
-    const offset = -index * logos[0].clientWidth;
-    slider.style.transform = `translateX(${offset}px)`;
+function changeImage() {
+    imageElement.style.opacity = 1;
+    setTimeout(() => {
+        imageElement.src = images[currentIndex];
+        currentIndex = (currentIndex + 1) % images.length;
+        imageElement.style.opacity = 1;
+    }, 500);
 }
 
-setInterval(slide, 1000); // Change slide every 5 seconds (adjust as needed)
+// Initial image change
+changeImage();
+
+// Change image every 3 seconds
+setInterval(changeImage, 3000);
+
+
+// Sidebar Code
+const toggleBtn = document.getElementById('toggleBtn');
+const toggleBtnText = document.getElementById('toggleBtnText');
+const sidebar = document.getElementById('sidebar');
+const closeSidebar = document.getElementById('closeSidebar');
+
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
+
+toggleBtnText.addEventListener('click', () => {
+    sidebar.classList.toggle('active');
+});
+
+closeSidebar.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+});
+
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    const isSidebar = target === sidebar || sidebar.contains(target);
+    const isToggleBtn = target === toggleBtn;
+    const isToggleBtnText = target === toggleBtnText;
+
+    if (!isSidebar && !isToggleBtn && !isToggleBtnText && sidebar.classList.contains('active')) {
+        sidebar.classList.remove('active');
+    }
+});
+
+
+// Scroll To Animation
+
+function scrollToSection(sectionId) {
+    const targetSection = document.getElementById(sectionId);
+
+    if (!targetSection) {
+      console.error(`Section with id '${sectionId}' not found.`);
+      return;
+    }
+
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: 'smooth'
+    });
+  }
